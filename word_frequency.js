a6c93d5044279ea2a1f5c8bdcc6e37e8aa7ec7f8
rakib <mdrakib991943@.com>
2026-02-05 22:39:13 +0600

    added analyzeMark

--- word_frequency.js
+++ word_frequency.js
@@ -1,17 +1,41 @@
-function wordFrequncy(sentence){
-    let wordCount={};
-let words = sentence.split(" ");
-for(let word of words){
-    let wordLowerCase = word.toLowerCase();
-   if(wordCount.hasOwnProperty(wordLowerCase)){
-    wordCount[wordLowerCase] ++ ;
-   }
-   else{
-    wordCount[wordLowerCase] =1 
-   }
-}
-return wordCount ; 
+
+
+// Problem 1 --------------------------
+
+// function wordFrequency(sentence){
+// let words = sentence.split(" ");
+// let wordCounts = {}
+// for(let word of words){
+//    let lowerWord = word.toLowerCase();
+//    if( wordCounts.hasOwnProperty(lowerWord)){
+//      wordCounts[lowerWord] ++;
+//    }else{
+//     wordCounts[lowerWord] = 1; 
+//    }
+// }
+// return wordCounts ; 
+// }
+
+// const result = wordFrequency("I never ever give up , i never give up , if i have to die i will die")
+// console.log(result);
+
+
+// Problem 2 ------------------------
+
+function analyzeMarks(marksObj){
+  let total = 0 ;
+  let count = 0 ;
+  let highest =Math.max(...Object.values(marksObj));
+  let lowest =Math.min(...Object.values(marksObj));
+  
+ for(let key in marksObj){
+   total += marksObj[key] ;
+   count ++;
+ }
+ const avarage = total / count ;
+return  {total, avarage,highest} ;
 }
 
-const result = wordFrequncy("I love JS and I love coding and JS is fun Fun")
+const result = analyzeMarks({math:79,english:65,physics:88,bangla:55})
+
 console.log(result);
